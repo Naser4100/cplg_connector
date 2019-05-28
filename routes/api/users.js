c86e78269ab7c8b8dd72014499579e04970a4c8c
Naser <nasersoftcorporation2013@gmail.com>
2019-05-28 21:02:37 +0600

    Login Registration with validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,8 @@ const passport = require("passport");
 // Load user model
 const User = require("../../models/Users");
 const keys = require("../../config/keys");
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // @route  GET /api/users/test
 // @desc   Test users Route
@@ -21,6 +23,13 @@ router.get("/test", (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
@@ -57,13 +66,21 @@ router.post("/register", (req, res) => {
 // @desc    Login user
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find the by email or check for the user
   User.findOne({ email: email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -81,7 +98,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password Incorrect!" });
+        errors.password = "Password Incorrect!";
+        return res.status(400).json(errors);
       }
     });
   });
